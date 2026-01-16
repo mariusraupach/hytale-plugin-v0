5342a726da56bbbe62311e93dbe0da2b1b543089
mariusraupach <mariusraupach@icloud.com>
2026-01-16 13:37:10 +0100

    Add command/Main.java

---
+++ src/main/java/io/github/mariusraupach/hytalepluginv0/command/Main.java
@@ -0,0 +1,28 @@
+package io.github.mariusraupach.hytalepluginv0.command;
+
+import com.hypixel.hytale.component.Ref;
+import com.hypixel.hytale.component.Store;
+import com.hypixel.hytale.protocol.packets.interface_.CustomPageLifetime;
+import com.hypixel.hytale.server.core.command.system.CommandContext;
+import com.hypixel.hytale.server.core.command.system.basecommands.AbstractPlayerCommand;
+import com.hypixel.hytale.server.core.entity.entities.Player;
+import com.hypixel.hytale.server.core.universe.PlayerRef;
+import com.hypixel.hytale.server.core.universe.world.World;
+import com.hypixel.hytale.server.core.universe.world.storage.EntityStore;
+import org.checkerframework.checker.nullness.compatqual.NonNullDecl;
+
+public class Main extends AbstractPlayerCommand {
+    public Main(@NonNullDecl String name, @NonNullDecl String description) {
+        super(name, description);
+    }
+
+    @Override
+    protected void execute(@NonNullDecl CommandContext commandContext, @NonNullDecl Store<EntityStore> store, @NonNullDecl Ref<EntityStore> ref, @NonNullDecl PlayerRef playerRef, @NonNullDecl World world) {
+        Player player = store.getComponent(ref, Player.getComponentType());
+
+        io.github.mariusraupach.hytalepluginv0.entity.Main main = new io.github.mariusraupach.hytalepluginv0.entity.Main(playerRef, CustomPageLifetime.CanDismiss);
+
+        assert player != null;
+        player.getPageManager().openCustomPage(ref, store, main);
+    }
+}

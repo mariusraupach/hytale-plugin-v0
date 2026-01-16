f6aa0cb61bf6f3a260e279b2882590c39919097d
mariusraupach <mariusraupach@icloud.com>
2026-01-16 13:37:23 +0100

    Modify build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,8 +13,24 @@ dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
+    compileOnly(files("libs/HytaleServer.jar"))
 }
 
 tasks.test {
     useJUnitPlatform()
+}
+
+tasks.jar {
+    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+
+    from("src/main/resources")
+
+    finalizedBy("copyJar")
+}
+
+tasks.register<Copy>("copyJar") {
+    from(tasks.jar)
+
+    into(File(System.getProperty("user.home"), "Library/Application Support/Hytale/UserData/Mods"))
 }
